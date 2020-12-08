2e0d2d4661198bcb9f85f276dd256c763ce2c5b8
James Kearl <james.kearl@gmail.com>
2020-12-08 14:29:30 -0700

    figured out how to draw the squares in the right location

--- empty-example/sketch.js
+++ empty-example/sketch.js
@@ -1,18 +1,54 @@
+let x = 400;
+let y = 400;
+let phi = 1.618033988749895;
+let toggle = 0;
+let height = 400;
+let width = height * phi;
+let goldenHeight = height;
+let goldenWidth = goldenHeight;
+let newLine = false;
+let goldenX = x;
+let goldenY = y;
+
 function setup() {
-  createCanvas(800, 800);
-  frameRate(1);
+  createCanvas(1600, 1600);
+  frameRate(12);
 }
+
+console.log(`start; height=${goldenHeight}, X=${goldenX}, Y=${goldenY}`);
 
 function draw() {
+  toggle = frameCount % 2;
   background(30, 100, 255);
-  const randos = [10, 30, 90, 140, 190, 210, 240, 270, 330, 340];
-  let x = 100;
-  let y = 100;
-  let height = 10 + frameCount * 2;
-  let width = height * 1.618033988749895;
-
   stroke(0);
   strokeWeight(2);
-  fill(0, 0, 200, 50);
-  rect(y, x, 400 * 1.618033988749895, 400);
+  noFill();
+  rect(x, y, width, height);
+  stroke(255);
+  rect(goldenX, goldenY, goldenWidth, goldenHeight);
+  if (toggle === 0) {
+    goldenY = goldenY + goldenHeight * phi;
+    goldenHeight = goldenHeight / phi;
+    goldenHeight = goldenHeight * -1;
+    goldenWidth = goldenHeight;
+    console.log(`odd; height=${goldenHeight}, X=${goldenX}, Y=${goldenY}`);
+    newLine = false;
+  } else {
+    goldenX = goldenX + goldenHeight * phi;
+    goldenHeight = goldenHeight / phi;
+    goldenWidth = goldenHeight * -1;
+    console.log(`even; height=${goldenHeight}, X=${goldenX}, Y=${goldenY}`);
+    newLine = true;
+  }
+  if (frameCount === 14) {
+    frameCount = 0;
+    goldenHeight = height;
+    goldenWidth = goldenHeight;
+    goldenX = x;
+    goldenY = y;
+  }
 }
+
+// function mouseClicked(event) {
+//   return false;
+// }
